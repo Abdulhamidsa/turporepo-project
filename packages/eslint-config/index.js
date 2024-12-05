de8c8eeb3763378b241f2ad2512d2b2671178141
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2024-12-05 14:57:17 +0100

    chore: migrate ESLint configuration to shared package and update scripts

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:jsx-a11y/recommended", "plugin:import/errors", "plugin:import/warnings"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y", "import"],
+  rules: {
+    "react/react-in-jsx-scope": "off", // Not needed in React 17+
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "import/order": ["error", { alphabetize: { order: "asc" } }],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
