622e60d8341007261d9d89bba4cad393903c4252
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2024-12-10 18:36:51 +0100

    feat: update Vite config to change UI alias path and set base URL

--- apps/vite-app/vite.config.ts
+++ apps/vite-app/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@repo/ui": path.resolve(__dirname, "../../packages/ui/src"),
+      "@repo/ui": "/path-to-your/ui/src",
     },
   },
+  base: "/",
   build: {
     outDir: "dist",
   },
