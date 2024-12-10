97be0525251781ec744d1ee26a00e403b8ece47b
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2024-12-10 18:39:50 +0100

    feat: update Vite config to use path.resolve for UI alias

--- apps/vite-app/vite.config.ts
+++ apps/vite-app/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@repo/ui": "/path-to-your/ui/src",
+      "@repo/ui": path.resolve(__dirname, "../../packages/ui/src"),
     },
   },
   base: "/",
