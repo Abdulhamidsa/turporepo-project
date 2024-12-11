e6c09cbee3b138486e0275107c782667b5aa3983
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2024-12-11 15:36:46 +0100

    feat: remove _id field from user credential model schema

--- apps/express-app/src/features/user/models/user.credential.model.ts
+++ apps/express-app/src/features/user/models/user.credential.model.ts
@@ -1,8 +1,6 @@
 import mongoose, { Schema, Model } from "mongoose";
 import { IUserCredential } from "@repo/data/types/UserType.ts";
 const userCredentialSchema: Schema<IUserCredential> = new Schema({
-  _id: { type: Schema.Types.String },
-
   email: {
     type: String,
     unique: true,
