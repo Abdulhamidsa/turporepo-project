ad9fc17846124739d615c9d666f3621fee53fd33
Abdulhamid <alsaati.abdulhamid@gmail.com>
2024-12-27 03:48:44 +0100

    feat: add user profile page route to application

--- apps/vite-app/src/App.tsx
+++ apps/vite-app/src/App.tsx
@@ -1,10 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Auth from "./features/auth";
+import ProfilePage from "./features/user/[username]/page";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/auth" element={<Auth />} />
+      <Route
+        path="/users/:username"
+        element={
+          <ProfilePage
+            params={{
+              username: "woddddsooww",
+            }}
+          />
+        }
+      />
     </Routes>
   );
 }
