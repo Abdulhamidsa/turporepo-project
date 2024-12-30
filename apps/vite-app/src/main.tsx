662642f810a16308413b4bf1d7a466eadc80dde2
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2024-12-30 13:26:31 +0100

    feat: remove Toaster component from main application entry point

--- apps/vite-app/src/main.tsx
+++ apps/vite-app/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "@repo/ui/globals.css";
-import { Toaster } from "@repo/ui/components/ui/toaster";
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Toaster />
     <BrowserRouter>
       <App />
     </BrowserRouter>
