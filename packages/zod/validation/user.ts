aa7f45939f3b3a961d085dde82010da602260d2e
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-01-01 05:05:49 +0100

    feat: update userProfileSchema to allow optional and nullable fields for username, bio, country, profession, profilePicture, and coverImage

--- packages/zod/validation/user.ts
+++ packages/zod/validation/user.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const userProfileSchema = z.object({
   _id: z.string().default(""), // Default to an empty string if missing
+  username: z.string().nullable().optional(), // Allow `null` or omit entirely
   mongo_ref: z.string().default(""), // Default
   profileComplete: z.boolean().default(false), // Default to `false`
   bio: z.string().nullable().optional(), // Allow `null` or omit entirely
@@ -9,6 +10,8 @@ export const userProfileSchema = z.object({
   country: z.string().nullable().optional(), // Allow `null` or omit entirely
   profession: z.string().nullable().optional(), // Allow `null` or omit entirely
   createdAt: z.string().default(new Date().toISOString()), // Default to the current date/time
+  profilePicture: z.string().nullable().optional(), // Allow `null` or omit entirely
+  coverImage: z.string().nullable().optional(), // Allow `null` or omit entirely
 });
 export type UserProfile = z.infer<typeof userProfileSchema>;
 // Parse the default profile
@@ -25,3 +28,13 @@ export type UserAuth = z.infer<typeof userAuthSchema>;
 
 // Parse the default user auth
 export const defaultUserAuth: UserAuth = userAuthSchema.parse({});
+
+export const UserProfileSchema = z.object({
+  username: z.string().min(3).optional(),
+  age: z.number().optional(),
+  bio: z.string().optional(),
+  profilePicture: z.string().optional(),
+  coverImage: z.string().optional(),
+  country: z.string().optional(),
+  profession: z.string().optional(),
+});
