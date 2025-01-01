ed745b7faeee9fed8166383e0998015991a130c3
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-01-01 05:09:40 +0100

    feat: add ApiResponse interface and extend UserProfile type with ProfileType and ProfileEditProps

--- packages/data/types/user.ts
+++ packages/data/types/user.ts
@@ -2,3 +2,19 @@ import { z } from "zod";
 import { userProfileSchema } from "../../zod/validation/user";
 
 export type UserProfile = z.infer<typeof userProfileSchema>;
+
+export type ProfileType = {
+  _id: string;
+  profileComplete: boolean;
+  username: string | null;
+  age: number | null;
+  bio: string | null;
+  profilePicture: string | null;
+  coverImage?: string | null;
+  country: string | null;
+  profession: string | null;
+  createdAt: string;
+};
+export type ProfileEditProps = {
+  friendlyId: string;
+};
