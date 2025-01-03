db0eec9e6689d6e8a813f888833a5339e67c469a
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-01-03 15:29:01 +0100

    refactor: update import path for useAuth hook in ProtectedRoute component

--- apps/vite-app/routes/protectedRoutes.tsx
+++ apps/vite-app/routes/protectedRoutes.tsx
@@ -1,7 +1,7 @@
 // src/routes/ProtectedRoute.tsx
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../src/features/user/hooks/use.auth";
 import Loading from "@repo/ui/components/ui/Loading";
 
 interface ProtectedRouteProps {
