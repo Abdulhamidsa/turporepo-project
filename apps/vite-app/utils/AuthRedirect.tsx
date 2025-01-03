048e79d9c97c75197a1eafbed1f129eab5d43c61
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-01-03 15:43:08 +0100

    feat: refactor AuthRedirect component to remove loading state handling and update import path for useAuth hook

--- apps/vite-app/utils/AuthRedirect.tsx
+++ apps/vite-app/utils/AuthRedirect.tsx
@@ -1,14 +1,10 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../src/features/user/hooks/use.auth";
 import Auth from "../src/features/auth";
-import Loading from "@repo/ui/components/ui/Loading";
 
 export function AuthOrRedirect() {
-  const { isAuthenticated, isLoading } = useAuth();
+  const { isAuthenticated } = useAuth();
 
-  if (isLoading) {
-    return <Loading />;
-  }
   if (isAuthenticated) {
     return <Navigate to="/" replace />;
   }
