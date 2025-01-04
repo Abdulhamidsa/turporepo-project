943376417defc5d9abab7718139fbce9115308f6
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-01-04 20:19:50 +0100

    feat: update authentication schemas with improved validation and defaults; add project schema for project management

--- packages/zod/validation/auth.ts
+++ packages/zod/validation/auth.ts
@@ -2,16 +2,19 @@ import { z } from "zod";
 
 // Signin schema
 export const signInSchema = z.object({
-  email: z.string().min(3, "Username must be at least 3 characters"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  email: z.string().email("Invalid email address"),
+  password: z.string(),
 });
 
 // Signup schema
 export const signUpSchema = z
   .object({
     email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
-    confirmPassword: z.string().min(6, "Password confirmation must match"),
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters long")
+      .regex(/^(?=.*[A-Z])|(?=.*\d)/, "Password must contain at least one uppercase letter or a number"),
+    confirmPassword: z.string().min(6, "Password confirmation must be at least 6 characters long"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords must match",
--- packages/zod/validation/user.ts
+++ packages/zod/validation/user.ts
@@ -1,17 +1,18 @@
 import { z } from "zod";
 
 export const userProfileSchema = z.object({
-  username: z.string().nullable().optional(),
-  profileComplete: z.boolean().optional(),
-  bio: z.string().nullable().optional(),
+  bio: z.string().nullable().default(""),
+  username: z.string().nullable().default(""),
   age: z.number().nullable().optional(),
-  countryOrigin: z.string().nullable().optional(),
-  profession: z.string().nullable().optional(),
-  profilePicture: z.string().nullable().optional(),
-  coverImage: z.string().nullable().optional(),
+  countryOrigin: z.string().nullable().default(""),
+  profession: z.string().nullable().default(""),
+  friendlyId: z.string().nullable().default(""),
+  profilePicture: z.string().nullable().default(""),
+  coverImage: z.string().nullable().default(""),
 });
 
 export type UserProfile = z.infer<typeof userProfileSchema>;
+
 // Parse the default profile
 export const defaultUserProfile: UserProfile = userProfileSchema.parse({});
 
@@ -36,3 +37,19 @@ export const defaultUserAuth: UserAuth = userAuthSchema.parse({});
 //   country: z.string().optional(),
 //   profession: z.string().optional(),
 // });
+
+// schemas.ts
+
+// Define the schema for a Project
+export const projectSchema = z.object({
+  id: z.string(),
+  title: z.string().min(1, "Title is required."),
+  description: z.string().min(1, "Description is required."),
+  url: z.string().url("Invalid URL format."),
+  media: z.array(z.object({ url: z.string().url("Invalid URL format") })),
+  thumbnail: z.string().url("Thumbnail is required."),
+  tags: z.array(z.string()), // Now expects array of strings
+});
+
+// Infer the TypeScript type from the schema
+export type ProjectFormData = z.infer<typeof projectSchema>;
