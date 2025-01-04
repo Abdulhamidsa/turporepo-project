32dfa6b88337653c422c92b81a6361c388efa178
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-01-04 21:48:03 +0100

    refactor: comment out Signout component code for future review

--- apps/vite-app/src/features/auth/components/Signout.tsx
+++ apps/vite-app/src/features/auth/components/Signout.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@repo/ui/components/ui/button";
-import { useAuth } from "../../../../context/AuthContext";
+// import { Button } from "@repo/ui/components/ui/button";
+// import { useAuth } from "../../../../context/AuthContext";
 
-function Signout() {
-  const { signOut } = useAuth();
+// function Signout() {
+//   const { signOut } = useAuth();
 
-  return <Button onClick={signOut}>Sign Out</Button>;
-}
+//   return <Button onClick={signOut}>Sign Out</Button>;
+// }
 
-export default Signout;
+// export default Signout;
